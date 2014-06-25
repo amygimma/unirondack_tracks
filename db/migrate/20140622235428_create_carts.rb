ec36eee0e7e6cedb7cc280fe4c7a5c71eab86163
amyg225 <amyg225@gmail.com>
2014-06-24 23:04:30 -0400

    fixing migrations for heroku

--- db/migrate/20140622235428_create_carts.rb
+++ db/migrate/20140622235428_create_carts.rb
@@ -1,7 +1,5 @@
 class CreateCarts < ActiveRecord::Migration
   def change
-    drop_table :carts
-    drop_table :shopping_carts
     create_table :carts do |t|
         t.integer :user_id
         t.integer :total
