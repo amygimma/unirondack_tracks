ce594dc17b4c8c1f6e020de4af091498f7942d6d
amyg225 <amyg225@gmail.com>
2014-06-25 20:34:54 -0400

    active admin config 2

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,7 @@
+//= require active_admin/base
+// Javascript related to the admin section
+//= require jquery
+// require jquery.validate
+// require jquery.tagsinput
+//= require active_admin/base
+
