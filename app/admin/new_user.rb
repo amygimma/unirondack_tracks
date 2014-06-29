42993d20752f0eec462c2d2942fec0fa2dad5ae3
andygimma <andy.n.gimma@gmail.com>
2014-06-28 20:40:03 -0400

    new admin pages

---
+++ app/admin/new_user.rb
@@ -0,0 +1,12 @@
+ ActiveAdmin.register_page "New User" do
+
+    sidebar :help do
+      ul do
+        li "To create a user, add the user name and password"
+      end
+    end
+
+    content do
+      para "User Form Here"
+    end
+  end
---
+++ app/admin/weekly_inventory.rb
@@ -0,0 +1,12 @@
+ ActiveAdmin.register_page "Weekly Inventory" do
+
+    sidebar :help do
+      ul do
+        li "First Line of Help"
+      end
+    end
+
+    content do
+      para "Hello World"
+    end
+  end
